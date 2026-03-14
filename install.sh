0ed7aab36bbee293a5071e18d3e65649488b39fc
Rexe <barmonrohit216@gmail.com>
2026-03-15 03:11:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-echo ""
-echo "🛠 Installing REXE Termux Tool Store..."
-echo ""
+clear
+echo "Installing REXE Hacker Store..."
 
 pkg update -y
 pkg upgrade -y
-pkg install python git -y
 
-pip install -r requirements.txt
+pkg install git -y
+pkg install python -y
+pkg install python-pip -y
+pkg install php -y
+pkg install curl -y
+pkg install wget -y
+pkg install nmap -y
+pkg install clang -y
+pkg install make -y
+pkg install golang -y
+
+pip install requests
+pip install colorama
 
 echo ""
-echo "✅ Installation Complete!"
-echo "Run: python rexe.py"
-echo ""
+echo "Installation Complete!"
+echo "Run Tool: python rexe.py"
